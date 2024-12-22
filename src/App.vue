fadac004ce128fa3ca6eaceff7e8d8bc2e88d6d9
Нурзада Алымжан-кызы <nurzadadev@gmail.com>
2024-12-23 05:48:30 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <div class="snow"></div>
+    <div class="santa">
+      <img src="/src/assets/santa.png" alt="santa">
+    </div>
+    <div class="video-wrapper">
+      <div class="video-frame">
+        <div class="frame-text">Spring Dependency Injection</div>
+        <video width="650" height="450" controls autoplay loop>
+          <source src="/src/assets/vvideo.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  </main>
+</template>
